1c469abb46b04ed1cc316b42442d0c40f0f46259
Mathieu Virbel <mat@kivy.org>
2017-05-05 18:31:11 +0200

    fixes library.references missing new line on newer project.properties

--- buildozer/targets/android.py
+++ buildozer/targets/android.py
@@ -820,6 +820,8 @@ class TargetAndroid(Target):
         # recreate the project.properties
         with io.open(project_fn, 'w', encoding='utf-8') as fd:
             fd.writelines((line.encode('utf-8') for line in content))
+            if not content[-1].endswith(u'\n'):
+                fd.write(u'\n')
             for index, ref in enumerate(references):
                 fd.write(u'android.library.reference.{}={}\n'.format(index + 1, ref))
 
