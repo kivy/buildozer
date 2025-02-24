52e400fe1b653e048e7fd1221df49ba2d2ca9fd9
Mathias Lindström <kuzeyron@gmail.com>
2025-02-24 11:05:03 +0200

    Support of display-cutout for android devices

--- buildozer/targets/android.py
+++ buildozer/targets/android.py
@@ -871,8 +871,8 @@ class TargetAndroid(Target):
             cmd.append("--home-app")
 
         # Enable display-cutout for Android devices
-        display_cutout = self.buildozer.config.getdefault('app', 'android.display_cutout', 'never')
-        if display_cutout in {'default', 'shortedges', 'shortEdges'}:
+        display_cutout = self.buildozer.config.getdefault('app', 'android.display_cutout', 'never').lower()
+        if display_cutout in {'default', 'shortedges'}:
             if display_cutout == 'shortedges':
                 display_cutout = 'shortEdges'
             cmd.append("--display-cutout={}".format(display_cutout))
